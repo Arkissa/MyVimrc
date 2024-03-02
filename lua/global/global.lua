ce3cce2091f1ffcfd4e96aabf084df5aaae25efb
New-arkssac <mrarkssac@gmail.com>
2024-03-02 12:34:37 +0800

    add type

--- lua/global/global.lua
+++ lua/global/global.lua
@@ -4,142 +4,121 @@ local ft, _ = vim
 ft = not ft and vim.fn.expand("%:e") or ft
 
 G = {
-    home = os.getenv("HOME"),
-    pwd = vim.fn.getcwd(),
-    folder = vim.split(vim.fn.system("basename `pwd`"), "\n")[1],
-    stdPath = vim.fn.stdpath("data"),
+    ---@type string
     vimrc = vim.fn.stdpath("config"),
-    imgUrl = "https://fishpi.cn/upload",
-    imgPath = "./img",
-    modeSuffix = ".lua",
-    fileType = ft,
-    lang = ({
-        lua = {
-            lsp = "lua_ls",
-            comm = "--"
-        },
-        go = {
-            lsp = "gopls",
-            comm = "//"
-        },
-        python = {
-            lsp = "pyright",
-            comm = "#"
-        },
-        c = {
-            lsp = "clangd",
-            comm = "//"
-        },
-        h = {
-            lsp = "clangd",
-            comm = "//"
-        },
-        json = {
-            lsp = "jsonls",
-            comm = "//"
-        },
-        haskell = {
-            lsp = "hls",
-            comm = "--"
-        }
+    ---@type string
+    mode_suffix = ".lua",
+    ---@type string
+    file_type = ft,
+    ---@type string
+    lsp = ({
+        lua = "lua_ls",
+        c = "clangd",
+        go = "gopls",
+        python = "pylsp",
+        haskell = "hls",
+        json = "jsonls",
     })[ft],
+    ---@type table
+    icon = {
+        ---@type string
+        Text = "",
+        ---@type string
+        Method = "󰆧",
+        ---@type string
+        Function = "󰊕",
+        ---@type string
+        Constructor = "",
+        ---@type string
+        Field = "󰇽", ---@type string
+        Variable = "󰂡",
+        ---@type string
+        Class = "󰠱",
+        Interface = "",
+        ---@type string
+        Module = "",
+        ---@type string
+        Property = "󰜢",
+        ---@type string
+        Unit = "",
+        ---@type string
+        Value = "󰎠",
+        ---@type string
+        Enum = "",
+        ---@type string
+        Keyword = "󰌋",
+        ---@type string
+        Snippet = "",
+        ---@type string
+        Color = "󰏘",
+        ---@type string
+        File = "󰈙",
+        ---@type string
+        Reference = "",
+        ---@type string
+        Folder = "󰉋",
+        ---@type string
+        EnumMember = "",
+        ---@type string
+        Constant = "󰏿",
+        ---@type string
+        Struct = "",
+        ---@type string
+        Event = "",
+        ---@type string
+        Operator = "󰆕",
+        ---@type string
+        TypeParameter = "󰅲",
+    },
 }
 
-G.icon = {
-    Text = "",
-    Method = "󰆧",
-    Function = "󰊕",
-    Constructor = "",
-    Field = "󰇽",
-    Variable = "󰂡",
-    Class = "󰠱",
-    Interface = "",
-    Module = "",
-    Property = "󰜢",
-    Unit = "",
-    Value = "󰎠",
-    Enum = "",
-    Keyword = "󰌋",
-    Snippet = "",
-    Color = "󰏘",
-    File = "󰈙",
-    Reference = "",
-    Folder = "󰉋",
-    EnumMember = "",
-    Constant = "󰏿",
-    Struct = "",
-    Event = "",
-    Operator = "󰆕",
-    TypeParameter = "󰅲",
-}
+if ft == "python" then
+    local venv = vim.fn.getenv("VIRTUAL_ENV")
+    G.pyvenv = (venv and venv or "")
+end
 
-function G:load(path, config)
-    local mod = {}
-    local files = G.filter(G.split(G.system("ls " .. path), "\n"), function(_, x)
-        return G.isdirectory(path .. "/" .. x) == 0
-    end)
-
-    local modules = G.filter(files, function(_, x)
-        return string.find(x, G.modeSuffix, #x - #G.modeSuffix)
-    end)
-
-    for _, v in ipairs(G.map(modules, function(_, x)
-        return config .. "." .. G.split(x, G.modeSuffix)[1]
-    end))
-    do
-        if v then
-            table.insert(mod, require(v))
+vim.schedule(function()
+    ---@type string
+    G.home = vim.fn.getenv("HOME")
+    ---@type string
+    G.pwd = vim.fn.getcwd()
+    ---@type string
+    G.stdPath = vim.fn.stdpath("data")
+    local folder = vim.fn.getcwd()
+    if type(folder) == "string" then
+        local path = string.byte("/", 1)
+        for i = #folder, 1, -1 do
+            if folder:byte(i) == path then
+                G.folder = folder:sub(i + 1)
+                return
+            end
         end
+    else
+        G.folder = ""
+    end
+end)
+
+---@param path string
+---@param config string
+function G:load(path, config)
+    local dir = io.popen("ls " .. path)
+    if not dir then
+        error("not found " .. path)
     end
 
-    return mod
-end
+    local mods = {}
+    local extend = ".lua"
+    for line in dir:lines() do
+        if not G.isdirectory(path .. "/" .. line) or line:find(extend, #line - #extend) == nil then
+            goto continue
+        end
 
-local wrapStr = {
-    ["("] = ")",
-    ["["] = "]",
-    ["{"] = "}",
-    ["<"] = ">"
-}
-
-local function getWrapStr(char)
-    if wrapStr[char] == nil then
-        return char, char
+        table.insert(mods, require(config .. "." .. G.split(line, extend)[1]))
+        ::continue::
     end
 
-    return char, wrapStr[char]
-end
-
-local function wrap(str, char)
-    local before, after = getWrapStr(char)
-    return before .. str .. after
-end
-
-local function getWord()
-    return G.expand("<cword>")
-end
-
-function G.getViusalLine()
-    local posStart, posEnd = G.getpos("'<"), G.getpos("'>")
-    local line = { posStart, posEnd }
-    table.sort(line, function(s, e)
-        return s[2] < e[2]
-    end)
-
-
-    return { line[0][2], line[0][3] }, { line[1][2], line[0][3] }
-end
-
-function G.blockWrap(char)
-
-end
-
-function G.wordWrap(char)
-    local word = getWord()
-    local line = G.getline('.')
-    local s, e = line:find(word)
-
-    G.setline(G.line('.'), line:sub(1, s - 1) .. wrap(word, char) .. line:sub(e + 1))
+    dir:close()
+    return mods
 end
 
 G = setmetatable(G, { __index = vim.fn })
