f4fe01abe066c3118047d511b347d3c9e12fe4c5
Arkssac <mrarkssac@gmail.com>
2025-01-09 22:35:09 +0800

    style: delete << and >> keymap

--- plugin/base/keymaps.lua
+++ plugin/base/keymaps.lua
@@ -1,9 +1,7 @@
 local opt = { silent = true, nowait = true }
 
-vim.keymap.set("v", ">", ">gv", opt)
-vim.keymap.set("v", "<", "<gv", opt)
 vim.keymap.set("v", "<C-c>", [["+y]], opt)
-vim.keymap.set("i", "<C-v>", "<C-R>+", opt)
+vim.keymap.set("i", "<C-v>", "<C-R><C-O>+", opt)
 -- vim.keymap.set("v", "p", '"_dP', opt)
 -- vim.keymap.set("n", "sD", ":bdelete<CR>", opt)
 vim.keymap.set("i", "<C-]>", "<C-X><C-]>", opt)
