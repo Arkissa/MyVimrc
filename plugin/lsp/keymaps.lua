f4b9826d85596a652a813e44a02ca81999341aee
Arkssac <mrarkssac@gmail.com>
2025-01-19 22:07:35 +0800

    style: abbreviation vim.lsp.buf to lsp

--- plugin/lsp/keymaps.lua
+++ plugin/lsp/keymaps.lua
@@ -1,5 +1,6 @@
 local set = vim.keymap.set
 local methods = vim.lsp.protocol.Methods
+local lsp = vim.lsp.buf
 
 local function with(f, args)
 	return function()
@@ -10,31 +11,31 @@ end
 local function attach_keymaps(client, bufnr)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
 	if client:supports_method(methods.textDocument_hover) then
-		set("n", "K", with(vim.lsp.buf.hover, { border = 'rounded', max_height = 30, max_width = 60 }), opts)
+		set("n", "K", with(lsp.hover, { border = 'rounded', max_height = 30, max_width = 60 }), opts)
 	end
 
 	if client:supports_method(methods.textDocument_rename) then
-		set("n", "<LEADER>r", vim.lsp.buf.rename, opts)
+		set("n", "<LEADER>r", lsp.rename, opts)
 	end
 
 	if client:supports_method(methods.textDocument_codeAction) then
-		set({ "n", "x" }, "<LEADER>a", vim.lsp.buf.code_action, opts)
+		set({ "n", "x" }, "<LEADER>a", lsp.code_action, opts)
 	end
 
 	if client:supports_method(methods.textDocument_typeDefinition) then
-		set("n", "gD", with(vim.lsp.buf.type_definition, { reuse_win = true }), opts)
+		set("n", "gD", with(lsp.type_definition, { reuse_win = true }), opts)
 	end
 
 	if client:supports_method(methods.textDocument_references) then
-		set("n", "g]", vim.lsp.buf.references, opts)
+		set("n", "g]", lsp.references, opts)
 	end
 
 	if client:supports_method(methods.textDocument_implementation) then
-		set("n", "<LEADER>i", vim.lsp.buf.implementation, opts)
+		set("n", "<LEADER>i", lsp.implementation, opts)
 	end
 
 	if client:supports_method(methods.textDocument_signatureHelp) then
-		set("i", "<C-s>", with(vim.lsp.buf.signature_help, { border = 'rounded', max_width = 60 }), opts)
+		set("i", "<C-s>", with(lsp.signature_help, { border = 'rounded', max_width = 60 }), opts)
 	end
 end
 
